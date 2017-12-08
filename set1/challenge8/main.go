ffd9c3c766df0e0fe17d04f9318deaa876ab0a77
joshuwi2 <joshuwi2@cisco.com>
2017-12-08 17:04:01 -0500

    Finished Set 1

--- set1/challenge8/main.go
+++ set1/challenge8/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"cryptopals/aes"
 	"fmt"
 	"os"
 )
@@ -17,11 +16,34 @@ func main() {
 	file, err := os.Open("./8.txt")
 	check(err)
 	defer file.Close()
+	total := 0
+	out := ""
 
 	fscanner := bufio.NewScanner(file)
 
 	for fscanner.Scan() {
 		src := fscanner.Text()
+		var blocks []string
+		tmp := 0
 
+		for i := 0; i < len(src)/32; i++ {
+			blocks = append(blocks, src[i*32:(i+1)*32])
+		}
+
+		for i, j := range blocks {
+			for k, l := range blocks {
+				if i != k {
+					if j == l {
+						tmp++
+					}
+				}
+			}
+		}
+		if tmp > total {
+			total = tmp
+			out = src
+		}
 	}
+
+	fmt.Println(out)
 }
